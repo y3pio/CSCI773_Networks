36fea3b1ea9b5557c1b30402d35bbf94f507812c
helloye <hello.paingy@gmail.com>
2020-04-30 20:17:35 -0400

    adding basic js functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello World!');
+const data = require('./data.json');
+
+// Maps/augment the weight and shortest_source node
+// Index 0 (first element) is assumed to be the source node.
+const networkNodes = data.nodes.map((d, i) => ({
+  ...d,
+  weight: i === 0 ? 0 : Infinity,
+  shortest_source: i === 0 ? d.id : undefined,
+}));
+
+// Flattens the edges array to a pretty format.
+const prettyPrintNetwork = (networkData) => {
+  console.clear();
+  const detailedTable = networkData.map(d => ({
+    ...d,
+    edges: d.edges.reduce((acc, e) => {
+      acc.push(`{${e.destination}:${e.cost}}`);
+      return acc;
+    }, []).join()
+  }));
+
+  console.table(detailedTable);
+}
+
+prettyPrintNetwork(networkNodes);
