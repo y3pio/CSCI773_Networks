465e197eaab2b68a21c9157e1257de2d52ef7ebb
helloye <hello.paingy@gmail.com>
2020-05-05 20:14:58 -0400

    Updates

---
+++ precache-manifest.1295357737555a86fd1a22ebde2538ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec401581abd22a2e5684c0ff7b3286bc",
+    "url": "/CSCI-773_Networks/index.html"
+  },
+  {
+    "revision": "735b9d59527880cc442d",
+    "url": "/CSCI-773_Networks/static/css/main.6f0e70f5.chunk.css"
+  },
+  {
+    "revision": "c1070e0c97fe53a94e6a",
+    "url": "/CSCI-773_Networks/static/js/2.05680127.chunk.js"
+  },
+  {
+    "revision": "887dee37bd7c75c2f81411b59ba3dbcf",
+    "url": "/CSCI-773_Networks/static/js/2.05680127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "735b9d59527880cc442d",
+    "url": "/CSCI-773_Networks/static/js/main.dce96b51.chunk.js"
+  },
+  {
+    "revision": "68193768ff72b01f7454",
+    "url": "/CSCI-773_Networks/static/js/runtime-main.60c47989.js"
+  },
+  {
+    "revision": "c6cdfded4630ba6d9a2dceb70aa4fe0f",
+    "url": "/CSCI-773_Networks/static/media/Roboto-Light.c6cdfded.ttf"
+  }
+]);
